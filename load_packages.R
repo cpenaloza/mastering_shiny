42f033bdd699e26cbbc6017579c3ab24450ba1b6
Claudia Penaloza <claudia.penaloza@conti-na.com>
2021-01-18 19:17:16 -0500

    Started shiny app book.

---
+++ load_packages.R
@@ -0,0 +1,5 @@
+install.packages(c(
+  "gapminder", "ggforce", "globals", "openintro", "RSQLite", "shiny", 
+  "shinycssloaders", "shinyFeedback", "shinythemes", "testthat", 
+  "thematic", "tidyverse", "vroom", "waiter", "xml2", "zeallot" 
+))
