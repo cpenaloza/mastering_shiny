60a80b9d70261a5b1fefe3369f1e68e2895b6ef2
Claudia Penaloza <claudia.penaloza@conti-na.com>
2021-02-13 19:58:32 -0500

    Trying to understand the end of ch5.

--- R/ch5.R
+++ R/ch5.R
@@ -250,7 +250,102 @@ population <- vroom::vroom("../neiss/population.tsv")
 # shinyApp(ui, server)
 
 
-# V4 ----
+# # V4 ----
+# 
+# count_top <- function(df, var, n = 5) {
+#   df %>%
+#     mutate({{ var }} := fct_lump(fct_infreq({{ var }}), n = n)) %>%
+#     group_by({{ var }}) %>%
+#     summarise(n = as.integer(sum(weight)))
+# }
+# 
+# 
+# ui <- fluidPage(
+#   fluidRow(
+#     column(8,
+#            selectInput("code", "Product",
+#                        choices = setNames(products$prod_code, products$title),
+#                        width = "100%"
+#            )
+#     ),
+#     column(2, selectInput("y", "Y axis", c("rate", "count"))),
+#     # column(2, sliderInput("length_table", "Table Length", min = 1L, max = 20L, value = 5L, step = 1L))
+#     column(2, selectInput("length_table", "Table Length", choices = 1:10, selected = 5))
+#   ),
+#   fluidRow(
+#     column(4, tableOutput("diag")),
+#     column(4, tableOutput("body_part")),
+#     column(4, tableOutput("location"))
+#   ),
+#   fluidRow(
+#     column(12, plotOutput("age_sex"))
+#   ),
+#   fluidRow(
+#     column(2, actionButton("story", "Tell me a story")),
+#     column(10, textOutput("narrative"))
+#   )
+# )
+# 
+# 
+# server <- function(input, output, session) {
+#   
+#   selected <- reactive(injuries %>% filter(prod_code == input$code))
+# 
+#   table_length <- reactiveVal()
+#   
+#   # Works with selectInput dropdown
+#   observeEvent(input$length_table, {
+#     table_length(as.numeric(input$length_table))
+#   })
+# 
+#   # # Works with sliderInput
+#     # observeEvent(input$length_table, {
+#   #   table_length(input$length_table)
+#   # })
+#   
+#   output$diag <- renderTable(count_top(selected(), diag, n = table_length()), width = "100%")
+#   output$body_part <- renderTable(count_top(selected(), body_part, n = table_length()), width = "100%")
+#   output$location <- renderTable(count_top(selected(), location, n = table_length()), width = "100%")
+#   
+#   
+#   summary <- reactive({
+#     selected() %>%
+#       count(age, sex, wt = weight) %>%
+#       left_join(population, by = c("age", "sex")) %>%
+#       mutate(rate = n / population * 1e4)
+#   })
+#   
+#   output$age_sex <- renderPlot({
+#     if (input$y == "count") {
+#       summary() %>%
+#         ggplot(aes(age, n, colour = sex)) +
+#         geom_line() +
+#         labs(y = "Estimated number of injuries")
+#     } else {
+#       summary() %>%
+#         ggplot(aes(age, rate, colour = sex)) +
+#         geom_line(na.rm = TRUE) +
+#         labs(y = "Injuries per 10,000 people")
+#     }
+#   }, res = 96)
+#   
+#   narrative_sample <- eventReactive(
+#     list(input$story, selected()),
+#     selected() %>% pull(narrative) %>% sample(1)
+#   )
+#   output$narrative <- renderText(narrative_sample())
+#   
+# }
+# 
+# shinyApp(ui, server)
+
+
+
+# Ex 1 ----
+# Ex 2 ----
+
+
+# Ex 3 ----
 
 count_top <- function(df, var, n = 5) {
   df %>%
@@ -269,8 +364,7 @@ ui <- fluidPage(
            )
     ),
     column(2, selectInput("y", "Y axis", c("rate", "count"))),
-    # column(2, sliderInput("length_table", "Table Length", min = 1L, max = 20L, value = 5L, step = 1L))
-    column(2, selectInput("length_table", "Table Length", choices = 1:10, selected = 5))
+    column(2, sliderInput("length_table", "Table Length", min = 1L, max = 20L, value = 5L, step = 1L))
   ),
   fluidRow(
     column(4, tableOutput("diag")),
@@ -288,20 +382,11 @@ ui <- fluidPage(
 
 
 server <- function(input, output, session) {
+  
   selected <- reactive(injuries %>% filter(prod_code == input$code))
 
-  table_length <- reactiveVal()
-  
-  # Works with selectInput dropdown
-  observeEvent(input$length_table, {
-    table_length(as.numeric(input$length_table))
-  })
+  table_length <- reactive(input$length_table)
 
-  # # Works with sliderInput
-    # observeEvent(input$length_table, {
-  #   table_length(input$length_table)
-  # })
-  
   output$diag <- renderTable(count_top(selected(), diag, n = table_length()), width = "100%")
   output$body_part <- renderTable(count_top(selected(), body_part, n = table_length()), width = "100%")
   output$location <- renderTable(count_top(selected(), location, n = table_length()), width = "100%")
@@ -340,7 +425,10 @@ shinyApp(ui, server)
 
 
 
-# Ex 1 ----
-# Ex 2 ----
-# Ex 3 ----
-# Ex 4 ----
+# Ex 4a ----
+# Provide a way to step through every narrative systematically with forward and backward buttons
+
+
+
+# Ex 4b ----
+# Make the list of narratives “circular” so that advancing forward from the last narrative takes you to the first.
